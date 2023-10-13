d9d09290175c11fa23b53b4175cfb0dcdfe447e4
Batsave <batsave.twitch@gmail.com>
2023-10-13 15:43:42 +0200

    Update Footer and NavBar

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,32 +1,43 @@
 import "../scss/components/footer.scss";
 
-
 export default function Footer() {
-    
   return (
     <footer className="footer">
       <div className="footer-container">
-        <ul className="section-list">
-          <li className="section-item">
-            <a href="#">Mentions Légales</a>
-          </li>
-          <li className="section-item">
-            <a href="#">CVG</a>
-          </li>
-          <li className="section-item">
-            <a href="#">Cookies</a>
-          </li>
-          <li className="section-item">
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-
-        <div className="footer-section">
-          <p className="copyright">
-            © 1992-2023 | ASSOCIATION LA CLAC. Tous droits réservés.
+        <a href="/" className="footer-logo"></a>
+        <div className="section">
+          <p className="section-title">A propos</p>
+          <p className="section-text">
+            {" "}
+            L'association LA CLAC est une association à but non lucratif qui
+            organise des événements ainsi que des spectacles de varietés les
+            deux derniers weekends complet de novembre.
           </p>
         </div>
+
+        <div className="section">
+          <p className="section-title">Légale</p>
+          <ul className="section-list">
+            <li className="section-item">
+              <a href="#">Mentions Légales</a>
+            </li>
+            <li className="section-item">
+              <a href="#">CVG</a>
+            </li>
+            <li className="section-item">
+              <a href="#">Cookies</a>
+            </li>
+            <li className="section-item">
+              <a href="#">Contact</a>
+            </li>
+          </ul>
         </div>
+        </div>
+      <div className="footer-section">
+        <p className="copyright">
+          © 1992-2023 | ASSOCIATION LA CLAC. Tous droits réservés.
+        </p>
+      </div>
     </footer>
   );
 }
