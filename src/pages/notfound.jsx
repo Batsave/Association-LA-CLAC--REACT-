581c406b6bb212712359a2a08c6b820b6611110f
Batsave <batsave.twitch@gmail.com>
2023-10-20 16:45:05 +0200

    Update DATA

--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <section>
       <div className="containererror">
-        <h1>Oops! Il semble que la page n'éxiste pas. </h1>
+        <h1>Oops! Il semble que la page n'existe pas.</h1>
         <p className="message">
           Nous vous conseillons de retourner à la page d'accueil.
         </p>
